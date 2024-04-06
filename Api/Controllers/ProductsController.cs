af8a25f31e40db38f67c48e54493f8b60b08b2c2
William Firmino <willian.firmino@hotmail.com>
2024-04-06 11:21:47 -0300

    End of Section 2

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+    public ProductsController(StoreContext context)
+    {
+            _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await _context.Products.ToListAsync();
+        return products;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# skinet project from Udemy course Learn to build an e-commerce app with .Net Core and Angular
+
